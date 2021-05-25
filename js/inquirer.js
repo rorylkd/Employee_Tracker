e7b33def766c34b17e3adb9adac319b265d4a4ac
rorylkd <rory.donaldson14@gmail.com>
2021-05-25 11:36:01 +0800

    Main menu inquirer prompt

--- js/inquirer.js
+++ js/inquirer.js
@@ -1,10 +1,22 @@
 const inquirer = require('inquirer');
 
-const mainMenu = [
+const mainMenu = 
     {
         type: 'list',
         name: 'mainMenuChoices',
         message: 'What would you like to do?',
         choices: ['Add departments, roles or employees', 'View departments, roles or employees', 'Update employee roles']
-    },
-]
+    }
+
+function mainMenuPrompt() {
+    inquirer.prompt(mainMenu).then((mainMenuAnswer) =>{
+        if (mainMenuAnswer === 'Add departments, roles or employees'){
+            console.log('You have chosen to add');
+        } else if (mainMenuAnswer === 'View departments, roles or employees') {
+            console.log('You have chosen to view');
+
+        } else if (mainMenuAnswer === 'Update employee roles') {
+            console.log('You have chosen to update');
+        }
+    })
+}
