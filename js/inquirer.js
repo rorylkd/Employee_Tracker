a08167bbb34db0cffb6d263be21a088f672f0427
rorylkd <rory.donaldson14@gmail.com>
2021-05-25 11:57:35 +0800

    Fixed mainmenu prompt

--- js/inquirer.js
+++ js/inquirer.js
@@ -1,22 +1,40 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
-const mainMenu = 
-    {
-        type: 'list',
-        name: 'mainMenuChoices',
-        message: 'What would you like to do?',
-        choices: ['Add departments, roles or employees', 'View departments, roles or employees', 'Update employee roles']
-    }
+const mainMenu = {
+  type: "list",
+  name: "mainMenuChoices",
+  message: "What would you like to do?",
+  choices: [
+    "Add departments, roles or employees",
+    "View departments, roles or employees",
+    "Update employee roles",
+  ],
+};
+
+function addPrompt() {
+  console.log("You have chosen to add departments, roles or employees");
+}
+
+function viewPrompt() {
+  console.log("You have chosen to view departments, roles or employees");
+}
+
+function updatePrompt() {
+  console.log("You have chosen to update employee roles");
+}
 
 function mainMenuPrompt() {
-    inquirer.prompt(mainMenu).then((mainMenuAnswer) =>{
-        if (mainMenuAnswer === 'Add departments, roles or employees'){
-            console.log('You have chosen to add');
-        } else if (mainMenuAnswer === 'View departments, roles or employees') {
-            console.log('You have chosen to view');
-
-        } else if (mainMenuAnswer === 'Update employee roles') {
-            console.log('You have chosen to update');
-        }
-    })
+  inquirer.prompt(mainMenu).then((mainMenuAnswer) => {
+    if (mainMenuAnswer.mainMenuChoices === "Add departments, roles or employees") {
+      addPrompt();
+    } else if (mainMenuAnswer.mainMenuChoices === "View departments, roles or employees") {
+      viewPrompt();
+    } else if (mainMenuAnswer.mainMenuChoices === "Update employee roles") {
+      updatePrompt();
+    } else {
+      console.log("Wrong answer");
+    }
+  });
 }
+
+mainMenuPrompt();
